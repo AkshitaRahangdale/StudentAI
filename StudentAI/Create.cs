ba1abb538402d78a4cc0935802235e21e724c61c
AkshitaRahangdale <akshitarahangdale@1720gmail.com>
2022-10-11 12:07:21 +0530

    Add project files.

---
+++ StudentAI/Create.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAI
+{
+    internal class Create
+    {
+        public int Roll { get; set; }
+        public string Name { get; set; }
+        public int Percent { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Roll}{Name}{Percent}";
+
+        }
+    }
+   
+
+}
---
+++ StudentAI/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Create c1=new Create();
+            c1.Name = "John";
+            c1.Roll = 12;
+            c1.Percent = 78;
+
+            Console.WriteLine(c1.Name + "" + c1.Percent);
+            Console.WriteLine(c1);
+
+            Create c2= new Create{ Roll = 12, Name = "John", Percent = 78 };
+            Console.WriteLine(c1);
+        }
+    }
+}
